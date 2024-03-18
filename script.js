f0f10a3e60c74e70e647a5df9af69cd024269eef
Nimrod <nimrodmandelakibet@gmail.com>
2024-03-18 18:36:39 +0100

    scrolling functionality

--- script.js
+++ script.js
@@ -50,3 +50,12 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+// Scrolling section functionality
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
