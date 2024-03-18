27c1b8c725ff42a7f920fd12e477da5c0dc83cf9
Nimrod <nimrodmandelakibet@gmail.com>
2024-03-18 15:00:28 +0100

    cookie implementation

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-///////////////////////////////////////
 // Modal window
 
 const modal = document.querySelector('.modal');
@@ -8,19 +7,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+// Open window
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
+// close window
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// Open modal on click
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
+// close modal on click
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -29,3 +32,21 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('section');
+
+// Cookie implementation
+const allButtons = document.getElementsByTagName('button');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved analytics. <button class="btn btn-close-cookie"> Got it!</button>';
+
+header.append(message);
+
+document
+  .querySelector('.btn-close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
