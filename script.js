07353e858917a3cb860f9dc2eae36c2325512edf
Nimrod <nimrodmandelakibet@gmail.com>
2024-03-19 13:40:07 +0100

    right slider for customer review

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  //section.classList.add('section--hidden');
 });
 
 // Lazy loading performance
@@ -164,7 +164,39 @@ const loadImage = function (entries, oberver) {
 const imgObserver = new IntersectionObserver(loadImage, {
   root: null,
   threshold: 0,
-  rootMargin: '200px'
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Sliders
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const slider = document.querySelector('.slider');
+
+slider.style.transform = 'scale(0.5)';
+slider.style.overflow = 'visible';
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+};
+btnRight.addEventListener('click', nextSlide);
